dbd70c11aa589357b4919c386aec9890ff7c1741
Arthur Le Moigne <arthur.lemoigne@gmail.com>
2015-02-23 14:09:07 +0100

    Add uglify to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,28 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      all: ["app/*.js"]
+    },
     browserify: {
       dist: {
         files: {
-          "build/app.js": [
-            "app/main.js"
-          ]
+          "build/app.js": ["app/main.js"]
         }
       }
     },
-    jshint: {
-      all: ["app/*.js"]
+    uglify: {
+      dist: {
+        files: {
+          "build/app.min.js": ["build/app.js"]
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('default', ['jshint', 'browserify']);
+  grunt.registerTask('default', ['jshint', 'browserify:dist', 'uglify:dist']);
 };
